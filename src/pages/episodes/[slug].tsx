66a3915d2cb7b560d446dfcdd12930fc3e60c74f
Márcio Rodrigues <marcio.mrcr@outlook.com>
2021-05-10 11:54:33 -0300

    final

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,15 +2,16 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
+
+import { useRouter } from 'next/router'
 import next, { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss';
-
 import { usePlayer } from '../../contexts/PlayerContext';
-import Head from 'next/head';
 
 type Episode = {
   id: string;
@@ -28,32 +29,32 @@ type EpisodeProps = {
   episode: Episode;
 }
 
-export default function Episode( { episode }: EpisodeProps) {
-    const { play } = usePlayer();
+export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
 
-    return (    
-     
+  return (
     <div className={styles.episode}>
       <Head>
-        <title>{episode.title} | Podcastr </title>
+        <title>{episode.title} | Podcastr</title>
       </Head>
 
       <div className={styles.thumbnailContainer}>
         <Link href="/">
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Voltar"/>
-        </button>
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
         </Link>
         <Image
-        width={700}
-        height={160}
-        src={episode.thumbnail}
-        objectFit="cover" 
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
         />
         <button type="button" onClick={() => play(episode)}>
-          <img src="/play.svg" alt="Tocar Episódio"/>
+          <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
+
       <header>
         <h1>{episode.title}</h1>
         <span>{episode.members}</span>
@@ -62,14 +63,13 @@ export default function Episode( { episode }: EpisodeProps) {
       </header>
 
       <div 
-      className={styles.description}
-      dangerouslySetInnerHTML={{ __html: episode.description}}
-      />    
-    </ div>      
+        className={styles.description} 
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
   )
 }
-// Monta o carregamento do conteúdo dinâmico da página estática no servidor Next
-// ou seja monta a pagina com os novos podcasts postados pra depois exibir a home
+
 export const getStaticPaths: GetStaticPaths = async () => {
   const { data } = await api.get('episodes', {
     params: {
@@ -81,20 +81,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const paths = data.map(episode => {
     return {
-       params: {
-         slug: episode.id
+      params: {
+        slug: episode.id
       }
-    }   
+    }
   })
 
   return {
     paths,
-    fallback: 'blocking' // só exibe o conteúdo depois de estar totalmente carregado
-  }  
+    fallback: 'blocking'
+  }
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { slug } = ctx.params
+  const { slug } = ctx.params;
 
   const { data } = await api.get(`/episodes/${slug}`)
 
@@ -112,8 +112,8 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   return {
     props: {
-      episode
+      episode,
     },
-    revalidate: 60 * 60 * 24  // 24 pra recarregar a página
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -1,9 +1,12 @@
 export function convertDurationToTimeString(duration: number) {
-    const hour = Math.floor(duration / 3600) // calculo de horas
-    const minutes = Math.floor((duration % 3600) / 60) // calculo de minutos
-    const seconds = duration % 60
+  const hours = Math.floor(duration / 3600)
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
 
-    const timeString = [hour, minutes, seconds].map(unit => String(unit).padStart(2, '0')).join(':')
+  const timeString = [hours, minutes, seconds]
+    .map(unit => String(unit).padStart(2, '0'))
+    .join(':')
 
-    return timeString
+  return timeString;
 }
+
